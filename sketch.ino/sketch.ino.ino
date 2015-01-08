df462b5acd97167f1ef7076a1e0dbbe96c78c014
cjmoeller <the.porenta@web.de>
2015-01-08 16:55:01 +0100

    Arduino sketch created

---
+++ sketch.ino/sketch.ino.ino
@@ -0,0 +1,13 @@
+//Motor communication Ports
+const int MOTOR1_E = 6;
+const int MOTOR1_M = 7;
+const int MOTOR2_E = 8;
+const int MOTOR2_M = 9;
+
+void setup(){
+  Serial.begin(9600); 
+}
+
+void loop() {
+  
+}
