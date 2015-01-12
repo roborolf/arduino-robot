f77c1f1252640b82520dc2679d835606d06516cb
Julius Möller <the.porenta@web.de>
2015-01-12 10:58:43 +0100

    Update sketch.ino

--- sketch.ino/sketch.ino
+++ sketch.ino/sketch.ino
@@ -11,3 +11,20 @@ void setup(){
 void loop() {
   
 }
+
+/**
+ *  Makes the robot drive using the specified MOTORS.
+ *  
+ *  speed
+ *    The drive speed in [0.0; 100.0]
+ *  
+ *  curvature
+ *    The curvature of the planned route    
+ *    in [-1.0; 1.0]
+ *    -1: maximum drive to the right
+ *     0: drive straightforward
+ *     1: maximum drive to the left
+ */
+void drive(float speed, float curvature){
+  
+}
